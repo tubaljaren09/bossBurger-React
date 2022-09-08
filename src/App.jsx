f3a3fdd72493a18e13d1e125c72f4d1dd4e2eac3
tubaljaren09 <tubaljaren09@gmail.com>
2022-09-08 12:36:37 +0800

    Add navbar and other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+
+// styles
+import "./styles/app.scss";
+
+// Components
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+// import Products from "./components/Products";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
 
 const App = () => {
-    return(
-        <div className="app-main">
-            <h1>Hello World</h1>
-        </div>
-    )
-}
+  return (
+    <div className="app-main">
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Images
+import Burger from "../assets/images/burger-icon.png";
+import Logo from "../assets/images/home-logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-main">
+      <div className="navbar-container">
+        <img src={Logo} alt="logo" />
+        <img src={Burger} alt="burger" width="40px" height="40px" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
