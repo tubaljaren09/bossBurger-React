6bdd55daeec1991b26ef90b09160836dd2be61f1
tubaljaren09 <tubaljaren09@gmail.com>
2022-09-08 21:41:14 +0800

    Add home page greetings and burger

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+// Images
+import BurgerImage from "../assets/images/home-hero-img.png";
+
 const Home = () => {
   return (
     <div className="home-main">
-      <h1>Home</h1>
+      <div className="home-container">
+        <div className="greetings">
+          <h1>
+            <span>boss</span> Burger
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet,
+            <br />
+            consectetur adipiscing elit.
+          </p>
+          <br />
+          <br />
+          <br />
+          <button>Order Now!</button>
+        </div>
+        <img src={BurgerImage} alt="burger-home" width="300px" height="220px" />
+      </div>
     </div>
   );
 };
