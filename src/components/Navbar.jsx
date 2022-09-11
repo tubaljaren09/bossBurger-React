f221a5c11509f0fd07ac3a2a4e3e0f06410648ef
tubaljaren09 <tubaljaren09@gmail.com>
2022-09-11 13:03:55 +0800

    Add navbar menu and replace icon of burger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Images
-import Burger from "../assets/images/burger-icon.png";
+import Burger from "../assets/images/solid-bars.png";
 import Logo from "../assets/images/home-logo.png";
+import Close from "../assets/images/close-icon.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const toggleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="navbar-main">
       <div className="navbar-container">
         <img src={Logo} width="100px" height="50px" alt="logo" />
-        <img src={Burger} alt="burger" width="40px" height="40px" />
+        <div onClick={toggleNav}>
+          {!nav ? (
+            <img src={Burger} alt="burger-icon" width="30px" height="25px" />
+          ) : (
+            <img src={Close} alt="close-icon" width="25px" height="25px" />
+          )}
+        </div>
       </div>
+      <ul className={!nav ? "menu-close" : "menu-show"}>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Contacts</li>
+      </ul>
     </div>
   );
 };
